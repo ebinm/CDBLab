4a0d71f460f1d2a2793712374afa5df7a8e156af
ge35qiq <ebin.madan@tum.de>
2021-12-07 23:12:30 +0100

    tthread sleep test

--- src/main/java/de/tum/i13/ecs/ECSLogic.java
+++ src/main/java/de/tum/i13/ecs/ECSLogic.java
@@ -63,6 +63,7 @@ public class ECSLogic {
             transferFrom = serverInfoPredecessor;
 
             connectionHandleThread.write(getMetaData());
+            Thread.sleep(1000);
             transfer(transferFrom, serverInfo, rangeCurrent);
         }
         connectionHandleThread.write("start");
